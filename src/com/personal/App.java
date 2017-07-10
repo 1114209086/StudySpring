f85a2bb2407126e398783aaf91042ad4bb1c7619
1114209086 <1114209086@qq.com>
2017-07-10 22:06:17 +0800

    Use simpleJdbcTemplate

--- src/com/personal/App.java
+++ src/com/personal/App.java
@@ -15,30 +15,34 @@ public class App {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext.xml");
-		Say say = (Say) context.getBean("sayBean");
-		say.println();
+//		Say say = (Say) context.getBean("sayBean");
+//		say.println();
 
-		CustomerDao customerDAO = (CustomerDao) context.getBean("customerDAO");
-		// Customer customer = new Customer(3, "yiibai",29);
-		// customerDAO.insert(customer);
+//		CustomerDao customerDAO = (CustomerDao) context.getBean("customerDAO");
+//		 Customer customer = new Customer(3, "yiibai",29);
+//		 customerDAO.insert(customer);
 
-		Customer customer1 = customerDAO.findByCustomerId(1);
-		System.out.println(customer1);
-		System.out.println(customerDAO.findAllCustomers());
+//		Customer customer1 = customerDAO.findByCustomerId(1);
+//		System.out.println(customer1);
+//		System.out.println(customerDAO.findAllCustomers());
+//
+//		Customer customer2 = new Customer(7, "yiibai1", 21);
+//		Customer customer3 = new Customer(8, "yiibai2", 22);
+//		Customer customer4 = new Customer(9, "yiibai3", 23);
+//
+//		List<Customer> customers = new ArrayList<Customer>();
+//		customers.add(customer2);
+//		customers.add(customer3);
+//		customers.add(customer4);
+//
+//		customerDAO.insertBatch(customers);
+//
+//		String sql = "UPDATE CUSTOMER SET NAME ='BATCHUPDATE'";
+//		customerDAO.insertBatchSQL(sql);
 
-		Customer customer2 = new Customer(7, "yiibai1", 21);
-		Customer customer3 = new Customer(8, "yiibai2", 22);
-		Customer customer4 = new Customer(9, "yiibai3", 23);
+		CustomerDao customerDAO = (CustomerDao) context.getBean("customerDAOSimple");
 
-		List<Customer> customers = new ArrayList<Customer>();
-		customers.add(customer2);
-		customers.add(customer3);
-		customers.add(customer4);
-
-		customerDAO.insertBatch(customers);
-
-		String sql = "UPDATE CUSTOMER SET NAME ='BATCHUPDATE'";
-		customerDAO.insertBatchSQL(sql);
+		System.out.println(customerDAO.findByCustomerId(2));
 	}
 
 }
---
+++ src/com/personal/dao/impl/CustomerDaoImplUseSimpleJdbcTemplate.java
@@ -0,0 +1,58 @@
+package com.personal.dao.impl;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.simple.ParameterizedBeanPropertyRowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
+
+import com.personal.dao.CustomerDao;
+import com.personal.model.Customer;
+import com.personal.model.CustomerRowMapper;
+
+@SuppressWarnings("deprecation")
+public class CustomerDaoImplUseSimpleJdbcTemplate extends SimpleJdbcDaoSupport
+		implements CustomerDao {
+
+	@Override
+	public void insert(Customer customer) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Customer findByCustomerId(int custId) {
+		String sql = "SELECT * FROM CUSTOMER WHERE CUST_ID = ?";
+
+		Customer customer = getSimpleJdbcTemplate().queryForObject(sql,
+				ParameterizedBeanPropertyRowMapper.newInstance(Customer.class), custId);
+		return customer;
+	}
+
+	public Customer findByCustomerId2(int custId) {
+		String sql = "SELECT * FROM CUSTOMER WHERE CUST_ID = ?";
+
+		Customer customer = getSimpleJdbcTemplate().queryForObject(sql,
+				ParameterizedBeanPropertyRowMapper.newInstance(Customer.class),
+				custId);
+		return customer;
+	}
+
+	@Override
+	public List<Customer> findAllCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertBatch(List<Customer> list) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insertBatchSQL(String sql) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
