4e15c67c3861a3146f9d6958ff9978a17f20df5f
1114209086 <1114209086@qq.com>
2017-07-11 22:06:02 +0800

    命名参数实例batch操作（另一个）

--- src/com/personal/dao/impl/CustomerDaoImplParamterBind.java
+++ src/com/personal/dao/impl/CustomerDaoImplParamterBind.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
 
 import com.personal.dao.CustomerDao;
@@ -47,6 +48,16 @@ public class CustomerDaoImplParamterBind extends SimpleJdbcDaoSupport implements
 			parameter.add(new BeanPropertySqlParameterSource(customer));
 		}
 		getSimpleJdbcTemplate().batchUpdate(sql, parameter.toArray(new SqlParameterSource[0]));
+
+	}
+	
+	public void insertBatch2(List<Customer> customers) {
+		   SqlParameterSource[] params = 
+					SqlParameterSourceUtils.createBatch(customers.toArray());
+					
+				   getSimpleJdbcTemplate().batchUpdate(
+					"INSERT INTO CUSTOMER (CUST_ID, NAME, AGE) VALUES (:custId, :name, :age)",
+					params);
 
 	}
 
